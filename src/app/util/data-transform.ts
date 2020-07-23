c55cdd159463cb408374f582cb93c5f821b22e57
Paninee K <zphingphong@gmail.com>
2020-07-22 22:52:00 -0700

    Fix data transform function

--- src/app/util/data-transform.ts
+++ src/app/util/data-transform.ts
@@ -3,14 +3,25 @@
  * @return JSON array;
  */
 
-const threatLevel = (data, filters): any[] => {
+import { DataFrame } from 'pandas-js';
 
+const threatLevel = (data, filters): any => {
   return {
     label: 'high',
     score: 60
   };
 };
 
+const newCases = (data, filters): number => {
+  return 189;
+};
+
+const ages = (data, filters): number => {
+  return 189;
+};
+
 export {
-  threatLevel
+  threatLevel,
+  newCases,
+  ages
 };
