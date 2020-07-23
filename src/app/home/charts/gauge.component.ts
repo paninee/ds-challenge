1d967f75cdf1b7288bbadb0cf201c2710973d9da
Paninee K <zphingphong@gmail.com>
2020-07-23 15:24:14 -0700

    Prepare guage UI and data

--- src/app/home/charts/gauge.component.ts
+++ src/app/home/charts/gauge.component.ts
@@ -12,6 +12,7 @@ am4core.useTheme(am4themes_animated);
 })
 export class GaugeComponent implements AfterViewInit, OnDestroy {
 	@Input() chartId: string;
+	@Input() threatLevel: {label: string, score: number} = {label: 'n/a', score: 0};
 	private chart: am4charts.GaugeChart;
 	public am4charts: any;
 
@@ -24,16 +25,35 @@ export class GaugeComponent implements AfterViewInit, OnDestroy {
     this.zone.runOutsideAngular(() => {
 			let chart = am4core.create(this.chartId, am4charts.GaugeChart);
 			chart.hiddenState.properties.opacity = 0; // this makes initial fade in effect
+			chart.fontSize = 11;
 			chart.innerRadius = am4core.percent(80);
+			chart.resizable = true;
 
 			const chartMin = 0;
 			const chartMax = 100;
-			const axis = chart.xAxes.push(new this.am4charts.ValueAxis());
-			axis.min = 0;
-			axis.max = 100;
+
+			var axis = chart.xAxes.push(new this.am4charts.ValueAxis());
+			axis.min = chartMin;
+			axis.max = chartMax;
 			axis.strictMinMax = true;
-			axis.renderer.grid.template.stroke = new am4core.InterfaceColorSet().getFor("background");
-			axis.renderer.grid.template.strokeOpacity = 0.3;
+			axis.renderer.radius = am4core.percent(80);
+			axis.renderer.inside = true;
+			axis.renderer.line.strokeOpacity = 0.1;
+			axis.renderer.ticks.template.disabled = false;
+			axis.renderer.ticks.template.strokeOpacity = 1;
+			axis.renderer.ticks.template.strokeWidth = 0.5;
+			axis.renderer.ticks.template.length = 5;
+			axis.renderer.grid.template.disabled = true;
+			axis.renderer.labels.template.radius = am4core.percent(15);
+			axis.renderer.labels.template.fontSize = "0.9em";
+
+			const axis2 = chart.xAxes.push(new this.am4charts.ValueAxis());
+			axis2.min = chartMin;
+			axis2.max = chartMax;
+			axis2.strictMinMax = true;
+			axis2.renderer.labels.template.disabled = true;
+			axis2.renderer.grid.template.stroke = new am4core.InterfaceColorSet().getFor("background");
+			axis2.renderer.grid.template.strokeOpacity = 0.3;
 
 			const data = {
 			  score: 52.7,
@@ -62,7 +82,7 @@ export class GaugeComponent implements AfterViewInit, OnDestroy {
 			};
 
 			for (let grading of data.gradingData) {
-			  var range = axis.axisRanges.create();
+			  var range = axis2.axisRanges.create();
 			  range.axisFill.fill = am4core.color(grading.color);
 			  range.axisFill.fillOpacity = 1;
 			  range.axisFill.zIndex = -1;
@@ -76,7 +96,39 @@ export class GaugeComponent implements AfterViewInit, OnDestroy {
 			  range.label.paddingBottom = -5; // ~half font size
 			  range.label.fontSize = "0.9em";
 			}
-			var hand = chart.hands.push(new am4charts.ClockHand());
+
+			const label = chart.radarContainer.createChild(am4core.Label);
+			label.isMeasured = false;
+			label.fontSize = "6em";
+			label.x = am4core.percent(50);
+			label.paddingBottom = 15;
+			label.horizontalCenter = "middle";
+			label.verticalCenter = "bottom";
+			//label.dataItem = data;
+			label.text = data.score.toFixed(1);
+			//label.text = "{score}";
+			label.fill = am4core.color('#ff0000');
+
+			const label2 = chart.radarContainer.createChild(am4core.Label);
+			label2.isMeasured = false;
+			label2.fontSize = "2em";
+			label2.horizontalCenter = "middle";
+			label2.verticalCenter = "bottom";
+			// label2.text = matchingGrade.title.toUpperCase();
+			// label2.fill = am4core.color(matchingGrade.color);
+			label2.text = 'Test';
+			label2.fill = am4core.color('#ff0000');
+
+			const hand = chart.hands.push(new am4charts.ClockHand());
+			hand.axis = axis2;
+			hand.innerRadius = am4core.percent(55);
+			hand.startWidth = 8;
+			hand.pin.disabled = true;
+			hand.value = data.score;
+			hand.fill = am4core.color("#444");
+			hand.stroke = am4core.color("#000");
+
+
 
       this.chart = chart;
     });
