3d90813f5e639d2236d9bfffdf57ffc4b45e353f
Paninee K <zphingphong@gmail.com>
2020-07-23 16:47:59 -0700

    refactor dounut

--- src/app/home/charts/dounut.component.ts
+++ src/app/home/charts/dounut.component.ts
@@ -61,14 +61,15 @@ export class DounutComponent implements AfterViewInit, OnDestroy {
 			let pieSeries = chart.series.push(new am4charts.PieSeries());
 			pieSeries.dataFields.value = "litres";
 			pieSeries.dataFields.category = "country";
+
 			pieSeries.slices.template.stroke = am4core.color("#fff");
 			pieSeries.slices.template.strokeWidth = 2;
 			pieSeries.slices.template.strokeOpacity = 1;
 
-			// This creates initial animation
-			pieSeries.hiddenState.properties.opacity = 1;
-			pieSeries.hiddenState.properties.endAngle = -90;
-			pieSeries.hiddenState.properties.startAngle = -90;
+			pieSeries.ticks.template.disabled = true;
+			pieSeries.alignLabels = false;
+			pieSeries.labels.template.text = "{value.percent.formatNumber('#.0')}%";
+
 
       this.chart = chart;
     });
