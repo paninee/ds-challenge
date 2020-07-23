b184aa6c43cf2aa1616fd8f6fd9f12ae35193455
Paninee K <zphingphong@gmail.com>
2020-07-22 21:44:09 -0700

    Prepare advance filter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+	public showAdvanceFilter: boolean;
+	
   constructor() { }
 
   ngOnInit() {
