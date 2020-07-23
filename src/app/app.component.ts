0bb20c112cf2ce5528ab7321dfceeb678bc386d2
Paninee K <zphingphong@gmail.com>
2020-07-22 19:18:48 -0700

    method to convert csv string to json array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { csvJSON } from './util/helpers'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  public userArray: any[] = [];
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+  	this.httpClient.get(`assets/OntarioCovid19Cases.csv`, {responseType: 'text'}).subscribe(data => {
+  		const json = csvJSON(data);
+  		console.log(json);
+  	},
+  	error => {
+  		console.log(error);
+  	});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
