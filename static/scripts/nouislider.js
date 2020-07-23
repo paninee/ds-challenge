3c8b52e1c3ba268b96f1d3db24b8be2a710eae4d
Paninee K <zphingphong@gmail.com>
2020-07-22 20:06:49 -0700

    delete old code

--- static/scripts/nouislider.js
+++
@@ -1,8 +0,0 @@
-var handlesSlider = document.getElementById('range');
-noUiSlider.create(handlesSlider, {
-    start: [4000, 8000],
-    range: {
-        'min': [2000],
-        'max': [10000]
-    }
-})
