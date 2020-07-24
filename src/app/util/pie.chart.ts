fd5ec44d13cd8c63c2bb63f31e40f41ce630b732
Paninee K <zphingphong@gmail.com>
2020-07-23 21:51:33 -0700

    dynamic color set for pie

--- src/app/util/pie.chart.ts
+++ src/app/util/pie.chart.ts
@@ -27,14 +27,12 @@ export class PieChartParentClass {
 		pieSeries.slices.template.stroke = am4core.color("#fff");
 		pieSeries.slices.template.strokeOpacity = 1;
 
-		pieSeries.colors.list = [
-		  am4core.color("#845EC2"),
-		  am4core.color("#D65DB1"),
-		  am4core.color("#FF6F91"),
-		  am4core.color("#FF9671"),
-		  am4core.color("#FFC75F"),
-		  am4core.color("#F9F871"),
-		];
+		let colorSet = [];
+		this.data.forEach(x => {
+			// Ex: #fff
+			colorSet.push(x.color);
+		});
+		pieSeries.colors.list = colorSet;
 
 		// This creates initial animation
 		pieSeries.hiddenState.properties.opacity = 1;
